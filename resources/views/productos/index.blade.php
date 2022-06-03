be0f19f46b16fa1ccb5b42068139065fab017752
Carlos Enrique Guizar Peña <71019682+Skullnietz@users.noreply.github.com>
2022-06-03 12:20:12 -0500

    Lista de productos con barra de busqueda

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -50,6 +50,10 @@ active
             </div>
             <!-- /.card-header -->
             <div class="card-body">
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Buscar producto" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+                  </form><br>
                 <div style="overflow-x:auto;">
                     <table class="table">
                         <thead>
